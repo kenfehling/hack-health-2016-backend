da97837c676b1928dfa21598600af24d9bf0d960
Ken Fehling <ken@androidideas.org>
2016-07-06 18:17:05 -0400

    OK

--- src/server.py
+++ src/server.py
@@ -10,11 +10,11 @@ app = Flask(__name__)
 
 @app.route('/register', methods=['POST'])
 @cross_origin()
-def hello_world():
-    name = request.form['name']
-    email = request.form['email']
-    resume = request.form['resume']
-    return {"success": True}
+def register():
+    #name = request.form['name']
+    #email = request.form['email']
+    #resume = request.form['resume']
+    return 'OK' #{"success": True}
 
 
 if __name__ == '__main__':

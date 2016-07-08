f7e2eb4aac49e544cb0dc15d1db4e4325017cc6d
Ken Fehling <ken@androidideas.org>
2016-07-08 15:02:17 -0400

    ok

--- test/test_db.py
+++ test/test_db.py
@@ -4,16 +4,16 @@ TEST_FILE = 'files/Resume.pdf'
 
 
 def test_save_record():
+    form_data = {
+        'name': 'Ken Fehling',
+        'email': 'ken@androidideas.org',
+        'gender': 'Male',
+        'year': 'Senior',
+        'tshirt': 'L',
+        'first_hackathon': 'No',
+        'num_hackathons': '3',
+        'theme': 'Fitness',
+        'idea': 'Not sure'
+    }
     with open(TEST_FILE, 'rb') as resume:
-        save_record({
-            'name': 'Ken Fehling',
-            'email': 'ken@androidideas.org',
-            'gender': 'Male',
-            'year': 'Senior',
-            'tshirt': 'L',
-            'first_hackathon': 'No',
-            'num_hackathons': '3',
-            'theme': 'Fitness',
-            'idea': 'Not sure'
-        }, resume=resume)
-
+        save_record(form_data=form_data, resume=resume)
